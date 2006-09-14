601ac3bfedede14480414f82a8ad030eaf59ddc6
orospakr <orospakr@002cd51d-1f0e-0410-beaa-fdc28df67276>
2006-09-14 23:29:51 +0000

    FCKeditor support!  Aside from a very annoying bug which causes it to get screwed up when used in Firefox when the server doesn't return the right MIME type for the fckeditor .xml files, it works nicely.  Read the README for more info on that bug.  I also dumbed down the options in the toolbar to prevent stupid users from creating definitions with ass-crazy markup in them.  Jake in particular comes to mind...
    git-svn-id: svn+ssh://infoman.ca/home/svn/tukia-rails@60 002cd51d-1f0e-0410-beaa-fdc28df67276

--- tukia/app/helpers/application_helper.rb
+++ tukia/app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 module ApplicationHelper
   def fckeditor_text_field(object, method, options = {})
     text_area(object, method, options ) +
-    javascript_tag( "var oFCKeditor = new FCKeditor('" + object + "[" + method + "]');oFCKeditor.ReplaceTextarea()" )
+    javascript_tag( "var oFCKeditor = new FCKeditor('" + object + "[" + method + "]');oFCKeditor.ToolbarSet = 'TukiaSane';oFCKeditor.ReplaceTextarea()" )
   end
 end
